bc305aa53f356f1560de5658feac0d70290b27bb
Antonio Sapuppo <antoniosapuppo@yahoo.it>
2021-03-19 09:23:03 +0100

    pan tilt on progress

--- scripts/base_pc.py
+++ scripts/base_pc.py
@@ -22,33 +22,44 @@ import sensor_msgs.msg as sensor_msgs
 from std_msgs.msg import Bool, String
 from geometry_msgs.msg import Twist
 
+
 # ---------------------------------------------------------------------
 # sites info:
 #
 
 
-def joy_front_camera(data):
-    rospy.loginfo(f'{rospy.get_caller_id()} joy_front_camera x:{data.linear.x} y:{data.linear.y}')
+class BasePc:
+    def __init__(self):
+        self.node_name = 'base_pc'
+        # registering node in ros master
+        moveit_commander.roscpp_initialize(sys.argv)
+        rospy.init_node(self.node_name, log_level=rospy.INFO)
+        # begin node code
+        rospy.loginfo(f'{self.node_name} Starting ')
+        self.cam_pan = 0        # zeroing pan tilt camera mount
+        self.cam_tilt = 0
+        # pub
+        self.pub_cam_pan = rospy.Publisher('/base/cam_pan', Int16, queue_size=10)
+        self.pub_cam_tilt = rospy.Publisher('/base/cam_tilt', Int16, queue_size=10)
+        # sub - Don't subscribe until everything has been initialized.
+        rospy.Subscriber("front_camera/cmd_vel", Twist, self.joy_front_camera)
+
+    @staticmethod
+    def spin():
+        rate = rospy.Rate(1)  # hz
+        while not rospy.is_shutdown():
+            rate.sleep()
+
+    @staticmethod
+    def joy_front_camera(data):
+        rospy.loginfo(f'{rospy.get_caller_id()}/joy_front_camera(x:{data.linear.x},y:{data.linear.y})')
 
 
 if __name__ == '__main__':
-    node_name = 'base_pc'
-    # registering node in ros master
-    moveit_commander.roscpp_initialize(sys.argv)
-    rospy.init_node(node_name, log_level=rospy.INFO)
-    # begin node code
-    rospy.loginfo(f'{node_name} Starting ')
-    # sub
-    rospy.Subscriber("front_camera/cmd_vel", Twist, joy_front_camera)
-    # pub
-    #pub_display8x8 = rospy.Publisher('/sensehat/led_panel', String, queue_size=10)
-    #
-    rate = rospy.Rate(1)  # hz
+    base_pc = BasePc()
     try:
-        while not rospy.is_shutdown():
-
-            rate.sleep()
+        base_pc.spin()
     except Exception as error:
-        rospy.logerr(f'{node_name} Error on Main: {error}')
+        rospy.logerr(f'{base_pc.node_name} Error on Main: {error}')
     except rospy.ROSInterruptException:
-        rospy.loginfo(f'{node_name} Shutdown')
+        rospy.loginfo(f'{base_pc.node_name} Shutdown')
