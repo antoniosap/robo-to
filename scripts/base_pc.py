5fcbd4f2f49e7ffbee338ae441f8ef6c11a55c13
Antonio Sapuppo <antoniosapuppo@yahoo.it>
2021-03-20 12:37:44 +0100

    pan tilt on progress

--- scripts/base_pc.py
+++ scripts/base_pc.py
@@ -19,7 +19,7 @@ import moveit_msgs.msg
 from math import pi, dist, fabs, cos
 from moveit_commander.conversions import pose_to_list
 import sensor_msgs.msg as sensor_msgs
-from std_msgs.msg import Bool, String
+from std_msgs.msg import Bool, String, Int16
 from geometry_msgs.msg import Twist
 
 
@@ -34,10 +34,15 @@ class BasePc:
         # registering node in ros master
         moveit_commander.roscpp_initialize(sys.argv)
         rospy.init_node(self.node_name, log_level=rospy.INFO)
+        rospy.on_shutdown(self.shutdown)
         # begin node code
         rospy.loginfo(f'{self.node_name} Starting ')
-        self.cam_pan = 0        # zeroing pan tilt camera mount
-        self.cam_tilt = 0
+        self.cam_pan = 90
+        self.cam_tilt = 90
+        self.scale_x = 1.0
+        self.scale_y = 1.0
+        self.cam_pan_range = (32, 120)
+        self.cam_tilt_range = (70, 185)
         # pub
         self.pub_cam_pan = rospy.Publisher('/base/cam_pan', Int16, queue_size=10)
         self.pub_cam_tilt = rospy.Publisher('/base/cam_tilt', Int16, queue_size=10)
@@ -45,14 +50,41 @@ class BasePc:
         rospy.Subscriber("front_camera/cmd_vel", Twist, self.joy_front_camera)
 
     @staticmethod
-    def spin():
+    def clamp(n, minn, maxn):
+        return max(min(maxn, n), minn)
+
+    def shutdown(self):
+        rospy.loginfo(f'{base_pc.node_name} Shutdown on progess')
+        self.home_off()
+        self.pub_cam_pan.publish(self.cam_pan)
+        self.pub_cam_tilt.publish(self.cam_tilt)
+
+    def home_off(self):
+        self.cam_pan = 90  # zeroing pan tilt camera mount
+        self.cam_tilt = 90
+
+    def home_on(self):
+        self.cam_pan = 90  # zeroing pan tilt camera mount
+        self.cam_tilt = 185
+        self.pub_cam_pan.publish(self.cam_pan)
+        self.pub_cam_tilt.publish(self.cam_tilt)
+
+    def spin(self):
+        rospy.sleep(0.1)
+        self.home_on()
         rate = rospy.Rate(1)  # hz
         while not rospy.is_shutdown():
             rate.sleep()
 
-    @staticmethod
-    def joy_front_camera(data):
+    def joy_front_camera(self, data):
         rospy.loginfo(f'{rospy.get_caller_id()}/joy_front_camera(x:{data.linear.x},y:{data.linear.y})')
+        self.cam_pan += int(data.linear.y * self.scale_y)
+        self.cam_pan = self.clamp(self.cam_pan, self.cam_pan_range[0], self.cam_pan_range[1])
+        self.cam_tilt += int(data.linear.x * self.scale_x)
+        self.cam_tilt = self.clamp(self.cam_tilt, self.cam_tilt_range[0], self.cam_tilt_range[1])
+        self.pub_cam_pan.publish(self.cam_pan)
+        self.pub_cam_tilt.publish(self.cam_tilt)
+        rospy.loginfo(f'{rospy.get_caller_id()}/current(x:{self.cam_pan},y:{self.cam_tilt})')
 
 
 if __name__ == '__main__':
